5ed93d07aff198fb763a7a08214813ef320b2351
BadHard101 <BadHard101@yandex.ru>
2024-08-19 00:25:00 +0300

    Initial commit

---
+++ src/test/java/com/sakura/link/SakuraLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakura.link;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakuraLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
