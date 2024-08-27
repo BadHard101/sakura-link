12f510d0b77de4cdf4c1d67e9902b92ced046065
BadHard101 <BadHard101@yandex.ru>
2024-08-27 18:58:17 +0300

    Added PostController:
    - Added ApiResponse
    - Added PostController
    - Fix PostServiceImplementation

--- src/main/java/com/sakura/link/service/PostServiceImplementation.java
+++ src/main/java/com/sakura/link/service/PostServiceImplementation.java
@@ -5,11 +5,13 @@ import com.sakura.link.models.User;
 import com.sakura.link.repository.PostRepository;
 import com.sakura.link.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class PostServiceImplementation  implements PostService {
 
     @Autowired
