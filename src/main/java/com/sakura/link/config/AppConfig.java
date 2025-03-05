d5cd631b0a1efe77ca9ef9757aac637ceee165ad
BadHard101 <BadHard101@yandex.ru>
2025-03-05 04:40:19 +0300

    изменил разрешенный в CORS адрес для дифолтного у Vite

--- src/main/java/com/sakura/link/config/AppConfig.java
+++ src/main/java/com/sakura/link/config/AppConfig.java
@@ -40,7 +40,8 @@ public class AppConfig {
             @Override
             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                 CorsConfiguration cfg = new CorsConfiguration();
-                cfg.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+//                cfg.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+                cfg.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
                 cfg.setAllowedMethods(Collections.singletonList("*"));
                 cfg.setAllowCredentials(true);
                 cfg.setAllowedHeaders(Collections.singletonList("*"));
